ffb31e569b17ab6f7b1ee9a3cf5c8dd6124f577c
Kuba Fietkiewicz <kuba.fietkiewicz@gmail.com>
2013-11-15 22:34:33 -0800

    cat endpoint

---
+++ routes/cat.js
@@ -0,0 +1,50 @@
+/*
+ * GET users listing.
+ */
+exports.list = function(req, res){
+
+  var googleapis = require('googleapis');
+
+  // This comes from ENV['GOOGLE_API_KEY']
+  // On heroku, set this with "heroku config:add GOOGLE_API_KEY=..."
+  API_KEY = process.env.GOOGLE_API_KEY
+
+
+  googleapis
+      .discover('youtube','v3')
+      .execute(function(err, client) {
+
+    var params = {
+      q: 'cat',
+      part: 'snippet', 
+      type: 'video',
+      videoEmbeddable: 'true',
+      maxResults: 20,
+      fields: 'items(id,snippet)'
+    }
+    
+    var req = client.youtube.search.list(params).withApiKey(API_KEY);
+
+
+    req.execute(function (err, response) {
+      //console.log('client', client);
+      //console.log('youtube response', response.items);
+      //console.log('error', err);
+
+      var rval = [];
+      
+      response.items.forEach(function(item, index) {
+        //console.log('item', item);
+        var cat = {
+          'id': item.id.videoId,
+          'image': item.snippet.thumbnails.high.url
+        };
+        rval.push(cat);
+      });
+
+      //console.log('rval',rval);
+
+      res.json(rval);
+    });
+  });
+};
